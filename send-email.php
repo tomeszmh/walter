0f07db3025cc076623d4051c5c8583c3e83d49ec
nyiritamas <nytomi90@gmail.com>
2026-04-13 21:30:36 +0200

    Add SMTP error detail to debug response

--- send-email.php
+++ send-email.php
@@ -154,5 +154,5 @@ try {
 
 } catch (Exception $e) {
     http_response_code(500);
-    echo json_encode(['success' => false, 'message' => 'server_error']);
+    echo json_encode(['success' => false, 'message' => $mail->ErrorInfo]);
 }
